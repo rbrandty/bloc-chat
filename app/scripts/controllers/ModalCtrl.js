0a43c98f79deecd407a4ea6b783661db4c62b03f
Rachel Brandt <rachel.brandt@gmail.com>
2017-05-21 15:55:52 -0400

    modalctrl file update, create chat rooms

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,29 +1,17 @@
 (function() {
-  function ModalCtrl($uibModal, $log, Room) {
+    function ModalCtrl($scope, $uibModalInstance){
+        $scope.text = "";
 
-    this.open = function() {
-      var modalInstance = $uibModal.open({
-        animation: this.animationsEnabled,
-        templateUrl : '/templates/modal.html',
-        controller: 'ModalCtrl',
-        controllerAs: 'modal'
+        $scope.ok = function(){
+            $uibModalInstance.close($scope.text);
+        };
 
-      });
+        $scope.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        }
+    }
 
-      modalInstance.result.then(function(name) {
-        this.room = name;
-        Room.create(this.room);
-      }, function() {
-        $log.info('Modal dismissed at ' + new Date());
-      });
-    };
-
-    this.toggleAnimation = function () {
-      this.animationsEnabled = !this.animationsEnabled;
-    };
-  }
-
-  angular
-  .module('bloc-chat')
-  .controller('ModalCtrl', ['$scope','Room', '$uibModalInstance', ModalCtrl]);
+    angular
+        .module('blocChat')
+        .controller('ModalCtrl', ['$scope', '$uibModalInstance', ModalCtrl])
 })();
